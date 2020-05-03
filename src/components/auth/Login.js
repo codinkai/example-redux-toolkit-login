61a99a235d3870f8ab9b3ba82adf3da030438e69
Kai Fehrs <kai@fehrs.org>
2020-05-03 14:10:08 +0200

    extracted LoginForm from Login component

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Alert, Button, Form, FormGroup, Input, Label, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Alert, Button, Form, FormGroup, Input, Label, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import { LogIn, LogOut } from 'react-feather';
 import { useSelector, useDispatch } from "react-redux";
 import {
@@ -11,33 +11,67 @@ import {
 import useInput from '../../hooks/useInput'
 
 
-const Login = () => {
+const LoginForm = (props) => {
   const dispatch = useDispatch();
 
   // redux state
-  const loggedIn = useSelector(isLoggedInSelector);
   const loading = useSelector(isLoadingSelector);
   const error = useSelector(errorSelector);
 
-  // state of login modal
-  const [modalOpened, setModalOpened] = useState(false);
-  const openModal = () => setModalOpened(true);
-  const closeModal = () => setModalOpened(false);
-
   // input hooks for form state
-  const { value: email, bind: bindEmail, reset: resetEmail } = useInput('');
-  const { value: password, bind: bindPassword, reset: resetPassword } = useInput('');
+  const { value: email, bind: bindEmail } = useInput('');
+  const { value: password, bind: bindPassword } = useInput('');
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
     dispatch(login({ email, password }));
   }
 
+  return (
+    <Form onSubmit={handleSubmit}>
+      {error &&
+        (<Alert color="danger">
+          {error}
+        </Alert>)
+      }
+      <ul>
+        <li>test@test.com</li>
+        <li>PASSWORD</li>
+      </ul>
+
+      <FormGroup>
+        <Label for="login-email">Email:</Label>
+        <Input type="email" name="email" id="login-email" {...bindEmail} disabled={loading} />
+      </FormGroup>
+      <FormGroup>
+        <Label for="login-password">Password:</Label>
+        <Input type="password" name="password" id="login-password" {...bindPassword} disabled={loading} />
+      </FormGroup>
+      <div className="float-right">
+        {loading === false ?
+          <Button color="primary" type="submit">Login</Button>
+          :
+          <Button disabled>Loading</Button>
+        }
+      </div>
+    </Form>
+  );
+};
+
+const Login = (props) => {
+  const dispatch = useDispatch();
+
+  // redux state
+  const loggedIn = useSelector(isLoggedInSelector);
+
+  // state of login modal
+  const [modalOpened, setModalOpened] = useState(false);
+  const openModal = () => setModalOpened(true);
+  const closeModal = () => setModalOpened(false);
+
   const handleLogout = () => {
-    closeModal();
-    resetEmail();
-    resetPassword();
     dispatch(logout());
+    closeModal();
   }
 
   // render logout button
@@ -58,38 +92,11 @@ const Login = () => {
       <Modal isOpen={modalOpened} >
         <ModalHeader>Login</ModalHeader>
         <ModalBody>
-          <Form onSubmit={handleSubmit}>
-            {error &&
-              (<Alert color="danger">
-                {error}
-              </Alert>)
-            }
-            <ul>
-              <li>test@test.com</li>
-              <li>PASSWORD</li>
-            </ul>
-
-            <FormGroup>
-              <Label for="login-email">Email:</Label>
-              <Input type="email" name="email" id="login-email" {...bindEmail} disabled={loading} />
-            </FormGroup>
-            <FormGroup>
-              <Label for="login-password">Password:</Label>
-              <Input type="password" name="password" id="login-password" {...bindPassword} disabled={loading} />
-            </FormGroup>
-            <div className="float-right">
-              {loading === false ?
-                <Button color="primary" type="submit">Login</Button>
-                :
-                <Button disabled>Loading</Button>
-              }
-              <Button className="ml-2" color="secondary" onClick={closeModal}>Cancel</Button>
-            </div>
-          </Form>
+          <LoginForm />
         </ModalBody>
       </Modal>
     </React.Fragment>
   );
-}
+};
 
 export default Login;
